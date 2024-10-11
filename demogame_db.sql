6ad0dc48c613d5a8974c19ed82085ae1c0575982
Tanvir Rahman Nibir <tanvirrahmannibir2004@gmail.com>
2024-10-11 20:56:47 +0600

    Update demogame_db.sql

--- demogame_db.sql
+++ demogame_db.sql
@@ -1,19 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               11.5.2-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             12.6.0.6765
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 
 -- Dumping database structure for airport_db
 CREATE DATABASE IF NOT EXISTS `airport_db` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
@@ -85,8 +69,3 @@ CREATE TABLE IF NOT EXISTS `quests` (
 INSERT INTO `quests` (`QuestID`, `QuestName`, `Description`, `Reward`, `LocationID`) VALUES
 	(1, 'First Flight', 'Travel to your first destination.', 100, 2);
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
